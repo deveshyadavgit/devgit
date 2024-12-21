cd1ecd2246dd1b3490d90ee546c0dbccb764c9b6
deveshyadavgit <deveshyadavgit@gmail.com>
2024-12-21 15:04:16 +0000

    dev9 complete

---
+++ devgit/dev9.js
@@ -0,0 +1,36 @@
+// # primitive
+
+// 7 type : string , boolearn , number , null , undefind , symbol , bigint, 
+
+const age = 100
+const agevalue = 100.3
+
+const isLoggedIn =false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol ('123')
+const anotherId = symbol ('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 345689974357890865436578n
+
+
+
+// reference (non primitive)
+
+// array , objects , functions
+
+const heros = ["shaktiman" , "naagraj" , "doga"]
+let myobj = {
+   name : "devesh",
+   age :22,
+}
+
+const myFunction = function (){
+   console.log("hello devesh");
+   }
+
+   console.log(typeof bigNumber  );
+   
